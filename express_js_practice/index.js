dc7335c65cd3892702362ca4a066522b7cea8c26
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-03-23 23:11:53 +0600

    different types of response

--- express_js_practice/index.js
+++ express_js_practice/index.js
@@ -1,8 +1,54 @@
 var express = require('express');
 app = express();
-app.get("/",function(req,res){
-    res.send("Hello Express Js");
-});
+
+//res.send()---- response er body k nirdesh kore
+    //res.end()---response ses obostha k nirdesh kore
+    //res.status(ekhane ekta status code bosbe)
+    app.get("/send",function(req,res){
+        res.send("Hello Express Js--it is send");
+        
+    });
+    app.get("/end",function(req,res){
+        res.end("Hello Express Js--it is end");
+        
+    });
+    app.get("/status",function(req,res){
+        res.status(401).end("Unauthor");
+        
+    });
+
+    app.get("/json",function(req,res){
+        var jsonArray = [
+            {
+                name:"Rafiqul",
+                age: "25"
+            },
+            {
+                name:"Rabiul",
+                age: "28"
+            }
+        ]
+        res.json(jsonArray);
+        
+    });
+
+    app.get("/download",function(req,res){
+        res.download("./upload/img.jpg");
+        
+    });
+
+    
+    app.get("/bangladesh",function(req,res){
+        res.redirect("http://localhost:8080/india");
+        
+    });
+
+    
+    app.get("/india",function(req,res){
+        res.send("This is india");
+        
+    });
+
 
 app.listen(8080, function(){
     console.log("Server run success")
