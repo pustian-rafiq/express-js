d6b80b1e97e617bd11c602661e13e3418f414a24
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-03-24 08:01:08 +0600

    post request

--- expressjs_request/index.js
+++ expressjs_request/index.js
@@ -1,12 +1,20 @@
 var express = require('express')
 
 app = express()
-//request query
-app.get('/', function(req,res){
-    let fname = req.query.firstname;
-    let lname = req.query.lastname;
+//get request query
+app.get('/header', function(req,res){
+    let fname = req.header('firstname');
+    let lname = req.header('lastname');
 
     res.end(fname +" "+ lname)
+});
+
+//post request query
+app.post('/', function(req,res){
+    let fname = req.header('firstname');
+    let pass = req.header('pass');
+
+    res.end(fname +" "+ pass)
 });
 
 app.listen(7070,function(){
